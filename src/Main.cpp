ff61a17140020161fe6369ccdb614b754607cfc6
Ben-Bingham <ben@benbingham.ca>
2024-06-14 13:25:55 -0600

    Added basic example in main

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+
+#include "GeometryPreprocessor.h"
+#include "renderers/OpenGlRenderer.h"
+#include "renderers/renderer.h"
+
+using namespace Rutile;
+
 int main() {
-    std::cout << "Hello World" << std::endl;
+    size_t width = 600;
+    size_t height = 400;
+
+    std::unique_ptr<Renderer> renderer = std::make_unique<OpenGlRenderer>();
+
+    renderer->Init();
+
+    GeometryPreprocessor geometryPreprocessor{ };
+
+    // geometryPreprocessor.add();
+    Bundle bundle = geometryPreprocessor.GetBundle(GeometryMode::OPTIMIZED);
+
+    renderer->Render(bundle, width, height);
+
+    renderer->Cleanup();
+
     std::cin.get();
 }
