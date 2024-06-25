c443d1a1ea4e62d29bc1ea62494510dc8300ae3e
Ben-Bingham <ben@benbingham.ca>
2024-06-24 21:57:47 -0600

    Modified ImGui Scaling

--- src/Main.cpp
+++ src/Main.cpp
@@ -236,6 +236,7 @@ int main() {
     ImGuiIO& io = ImGui::GetIO();
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
     io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+    io.FontGlobalScale = 2.0f;
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init();
