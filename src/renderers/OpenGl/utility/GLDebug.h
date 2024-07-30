231510abbc1e99b5e3ae725f8774af948e97af62
Ben-Bingham <ben@benbingham.ca>
2024-07-30 17:19:16 -0600

    Trying to fix folder names

--- src/renderers/OpenGl/utility/GLDebug.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <gl/glew.h>
-#include <GLFW/glfw3.h>
-
-void APIENTRY glDebugOutput(GLenum source, GLenum type, unsigned int id, GLenum severity,
-    GLsizei length, const char* message, const void* userParam);
