02da7ccbbaabb001d56421fec1b2bdc846901ad5
Ben-Bingham <ben@benbingham.ca>
2024-11-08 19:35:14 -0700

    Made n more reasonable

--- src/Renderers/Voxel-Ray-Tracing/VoxelRayTracing.cpp
+++ src/Renderers/Voxel-Ray-Tracing/VoxelRayTracing.cpp
@@ -553,7 +553,7 @@ namespace Rutile {
          */
 
         AABB sceneBoundingBox{ glm::vec3{ -0.1f }, glm::vec3{ 0.1f } };
-        constexpr int n = 1024;
+        constexpr int n = 256;
 
         for (auto obj : App::scene.objects) {
             sceneBoundingBox = AABBFactory::Construct(AABBFactory::Construct(obj), sceneBoundingBox);
