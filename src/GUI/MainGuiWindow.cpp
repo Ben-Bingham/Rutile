7dfa10dbfe3aad5c0e43323279c88aa93e577312
Ben-Bingham <ben@benbingham.ca>
2025-09-06 14:39:47 -0600

    Removed unused gui files

--- src/GUI/MainGuiWindow.cpp
+++ src/GUI/MainGuiWindow.cpp
@@ -1,5 +1,4 @@
 #include "MainGuiWindow.h"
-#include "GeneralSettings.h"
 
 #include "imgui.h"
 #include "RayTracing.h"
@@ -7,7 +6,6 @@
 #include "ShadowSettings.h"
 
 #include "SceneObjects.h"
-#include "TimingStatistics.h"
 
 #include "Settings/App.h"
 
@@ -18,13 +16,6 @@ namespace Rutile {
 
         ImGui::Begin("Rutile");
         {
-            if (ImGui::CollapsingHeader("General", ImGuiTreeNodeFlags_DefaultOpen)) {
-                GeneralSettings();
-            }
-
-            if (ImGui::CollapsingHeader("Timing Statistics", ImGuiTreeNodeFlags_DefaultOpen)) {
-                TimingStatistics();
-            }
 
             if (ImGui::CollapsingHeader("Rendering Settings")) {
                 RenderingSettings();
